001bf356417bca88d6a027e33b8c0492c2ca0f2d
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2021-05-03 21:20:54 +0300

    Add files via upload

---
+++ constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0XFF1D1E33);
+const kInactiveCardColor = Color(0XFF111328);
+const kBottomContainerColor = Color(0XFFEB1555);
+const kTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0XFF8D8E98),
+);
+
+const kNumberStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold
+);
+
+const kBmiTextStyle = TextStyle(
+  fontSize: 100,
+  fontWeight: FontWeight.bold
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22
+);
